e05775ab61f8b8ad11dc7b28b55fed4e0b0c361d
Miriam Reina <m.reinabolanos@gmail.com>
2023-04-26 20:42:47 +0200

    Comienzo guia de estilos

--- src/styleguide/views-sg/Styleguide.jsx
+++ src/styleguide/views-sg/Styleguide.jsx
@@ -1,9 +1,20 @@
 import Nav from "../Nav";
 import "./styleguide.scss";
+import Buttons from "../../components/button/Buttons";
 
 export default function Styleguide() {
-  <>
-    return <h1>Hola, soy la styleguide</h1>
-    <Nav />
-  </>;
+  return (
+    <>
+      <div className="section">
+        <h1>soy un h1</h1>
+        <h2>soy un h2</h2>
+        <h3>soy un h3</h3>
+        <h4>soy un h4</h4>
+        <h5>soy un h5</h5>
+        <h6>soy un h6</h6>
+      </div>
+      <Nav />
+      <Buttons />
+    </>
+  );
 }
