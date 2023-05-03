f4222b0f80ef62cad78c024ccf10fd6396808ba0
Miriam Reina <m.reinabolanos@gmail.com>
2023-05-03 20:05:20 +0200

    variables de colores

--- src/styleguide/views-sg/Styleguide.jsx
+++ src/styleguide/views-sg/Styleguide.jsx
@@ -16,21 +16,67 @@ export default function Styleguide() {
 
       <div className="section">
         <h3>Variable Colors</h3>
-        <div className="color__circle">
-          <p>50</p>
-          <p>#FDEDE7</p>
-        </div>
-        <div className="color__circle">
-          <p>100</p>
-          <p>#FCDCCF</p>
-        </div>
-        <div className="color__circle">
-          <p>200</p>
-          <p>#F9B99F</p>
-        </div>
-        <div className="color__circle">
-          <p>300</p>
-          <p>##F7A787</p>
+        <div className="section__colors">
+          <div className="color__circle color__circle--mo-50">
+            <p>
+              <strong>50</strong>
+            </p>
+            <p>#FDEDE7</p>
+          </div>
+          <div className="color__circle color__circle--mo-100">
+            <p>
+              <strong>100</strong>
+            </p>
+            <p>#FCDCCF</p>
+          </div>
+          <div className="color__circle color__circle--mo-200">
+            <p>
+              <strong>200</strong>
+            </p>
+            <p>#F9B99F</p>
+          </div>
+          <div className="color__circle color__circle--mo-200">
+            <p>
+              <strong>300</strong>
+            </p>
+            <p>#F7A787</p>
+          </div>
+          <div className="color__circle color__circle--mo-400">
+            <p>
+              <strong>400</strong>
+            </p>
+            <p>#F7A787</p>
+          </div>
+          <div className="color__circle color__circle--mo-500">
+            <p>
+              <strong>500</strong>
+            </p>
+            <p>#F7A787</p>
+          </div>
+          <div className="color__circle color__circle--mo-600">
+            <p>
+              <strong>600</strong>
+            </p>
+            <p>#F7A787</p>
+          </div>
+          <div className="color__circle color__circle--mo-700">
+            <p>
+              <strong>700</strong>
+            </p>
+            <p>#F7A787</p>
+          </div>
+          <div className="color__circle color__circle--mo-800">
+            <p>
+              <strong>800</strong>
+            </p>
+            <p>#F7A787</p>
+          </div>
+          <div className="color__circle color__circle--mo-900">
+            <p>
+              <strong>900</strong>
+            </p>
+            <p>#F7A787</p>
+          </div>
         </div>
       </div>
       <Nav />
