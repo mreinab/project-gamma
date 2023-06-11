62f0c251909c80c1f489fc36f3a78728aedd4dca
Miriam Reina <m.reinabolanos@gmail.com>
2023-06-11 10:27:58 +0200

    prueba de rutas

--- src/components/backgroundvideo/BackgroundVideo.jsx
+++ src/components/backgroundvideo/BackgroundVideo.jsx
@@ -6,7 +6,7 @@ const BackgroundVideo = () => {
     <div className="main__video--container">
       <div className="overlay"></div>
       <video autoPlay loop muted>
-        <source src="./src/assets/06_1596083776.mp4" type="video/mp4" />
+        <source src="../../src/assets/06_1596083776.mp4" type="video/mp4" />
       </video>
     </div>
   );
