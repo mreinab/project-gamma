6b45ad23448f5949b98bf8d726528d8f9105b626
Miriam Reina <m.reinabolanos@gmail.com>
2023-06-14 11:26:11 +0200

    card3d included

--- src/components/Eclipse/Eclipse.jsx
+++ src/components/Eclipse/Eclipse.jsx
@@ -9,6 +9,11 @@ function Eclipse() {
       </div>
 
       <img src="./main-eclipse2.svg" alt="Eclipse" className="img--eclipse" />
+      <div className="cardContainer">
+        <video autoPlay loop muted className="card3d">
+          <source src="../../../public/card3D.mp4" type="video/mp4" />
+        </video>
+      </div>
 
       <div className="bg__img--container--lines--right">
         <img src="./main-lines.svg" className="bg__img--lines" />
